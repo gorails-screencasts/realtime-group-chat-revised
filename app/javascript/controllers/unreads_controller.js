d01dd775eb00dab8af51cae9a6583521db63a5f6
Chris Oliver <excid3@gmail.com>
2020-07-09 15:45:42 -0500

    Add mentions count

--- app/javascript/controllers/unreads_controller.js
+++ app/javascript/controllers/unreads_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "stimulus"
 import consumer from "channels/consumer"
 
 export default class extends Controller {
+  static targets = ["mentions"]
+
   connect() {
     this.username = document.querySelector("meta[name='username']").getAttribute("content")
     this.subscription = consumer.subscriptions.create({ channel: "UnreadsChannel", id: this.data.get("id") }, {
@@ -25,6 +27,9 @@ export default class extends Controller {
     this.element.classList.add("font-weight-bold")
 
     if (data.mentions && data.mentions.includes(this.username)) {
+      let count = parseInt(this.mentionsTarget.textContent)
+      this.mentionsTarget.textContent = count + 1
+      this.mentionsTarget.removeAttribute("hidden")
       this.notify(data.body)
     }
   }
